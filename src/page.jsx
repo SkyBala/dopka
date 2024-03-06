dcb6c6528ceddb43dfccfeb3fd6641061cde5c69
Obemych <beksultanovich.911@gmail.com>
2024-03-06 19:23:46 +0600

    8

---
+++ src/page.jsx
@@ -0,0 +1,26 @@
+import HTMLFlipBook from "react-pageflip";
+
+import geeks from "./images/photo_2023-02-01_00-37-45.jpg"
+import PageAyana from "./pageAyan";
+import PageMilana from "./pageMilana";
+import PageAiperi from "./pageAuperi";
+import PageAidana from "./pageAidana";
+
+function Book(props) {
+  return (
+    <HTMLFlipBook width={550} height={750}>
+      <div className="demoPage1">
+      <img src={geeks} alt="geeks"/>
+      <h1>
+        С 8 марта вас!!!
+      </h1>
+      </div>
+    
+      <div className="demoPage"><PageAyana/></div>
+      <div className="demoPage"><PageMilana/></div>
+      <div className="demoPage"><PageAiperi/></div>
+      <div className="demoPage"><PageAidana/> </div>
+    </HTMLFlipBook>
+  );    
+}
+export default Book;
---
+++ src/pageAuperi.jsx
@@ -0,0 +1,37 @@
+
+
+import React,{ useState, useRef } from 'react'
+import music from "./ty.mp3"
+
+function PageAiperi() {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const audioPlayer = useRef(null);
+  
+    const togglePlay = () => {
+      if (!isPlaying) {
+        audioPlayer.current.play();
+      } else {
+        audioPlayer.current.pause();
+      }
+      setIsPlaying(!isPlaying);
+    };
+  
+    const stopPlayback = () => {
+      audioPlayer.current.pause();
+      audioPlayer.current.currentTime = 0;
+      setIsPlaying(false);
+    };
+  
+  return (
+    <div>
+        <h1 className='audioH1'>Aiperi</h1>
+    <audio ref={audioPlayer} src={music} />
+    <div>
+      <button onClick={togglePlay}>{isPlaying ? 'Pause' : 'Play'}</button>
+      <button onClick={stopPlayback}>Stop</button>
+    </div>
+    </div>
+  )
+}
+
+export default PageAiperi
